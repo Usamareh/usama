3fab15ceab7592a68ddcac008bc72afc8b7a344d
fa14-bse-227 <fa14-bse-227@LAB3B-10.cns.ciit>
2017-10-23 10:24:57 +0500

    lab sessional

---
+++ calculator/src/calculator/Cost_cal.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Cost_cal {
+
+	public static void main(String[] args) {
+		double w = 0 ;      //weight is denoted as w
+        double c;      //cost is denoted as c
+        if(w<=5)
+        {
+        	c =w*0.75;
+        	
+        }
+        else if(w > 5){
+        	c =w*0.5;
+        }
+        return;
+	}
+
+}
